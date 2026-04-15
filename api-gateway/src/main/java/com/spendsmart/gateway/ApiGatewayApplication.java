f4799d58fee3b294e9aa8c5d630a03d368f4370d
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-04-15 21:17:41 +0530

    [Harsh Raj Singh] : configured eureka server

--- api-gateway/src/main/java/com/spendsmart/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/spendsmart/gateway/ApiGatewayApplication.java
@@ -3,8 +3,9 @@ package com.spendsmart.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
--- auth-service/src/main/java/com/spendsmart/AuthServiceApplication.java
+++ auth-service/src/main/java/com/spendsmart/AuthServiceApplication.java
@@ -2,8 +2,9 @@ package com.spendsmart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
